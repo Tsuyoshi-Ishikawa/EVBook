56439b72543bf5defbdfcd13db53d4201dd92a27
vagrant <vagrant@localhost.localdomain>
2020-05-16 11:19:28 +0900

    routing change group

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,27 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/home', 'UsersController@home')->name('home')->middleware('auth'); //showページ
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', 'UsersController@home');
+    Route::get('/words/create', 'WordsController@create');
+    Route::post('/words', 'WordsController@store');
+    Route::get('/words/{word}/edit', 'WordsController@edit');
+    Route::put('/words/{word}', 'WordsController@update');
+    Route::delete('/words/{id}', 'WordsController@destroy');
+    Route::get('/words/test', 'WordsController@test');
+    Route::get('/words/index', 'WordsController@index');
+    Route::post('/words/{id}/like', 'WordsController@like');
+});
 
-Route::get('/words/create', 'WordsController@create')->middleware('auth');
-Route::post('/words', 'WordsController@store')->middleware('auth');
-Route::get('/words/{word}/edit', 'WordsController@edit')->middleware('auth');
-Route::put('/words/{word}', 'WordsController@update')->middleware('auth');
-Route::delete('/words/{id}', 'WordsController@destroy')->middleware('auth');
+// Route::get('/home', 'UsersController@home')->middleware('auth'); //showページ
 
-Route::get('/words/test', 'WordsController@test')->middleware('auth');
+// Route::get('/words/create', 'WordsController@create')->middleware('auth');
+// Route::post('/words', 'WordsController@store')->middleware('auth');
+// Route::get('/words/{word}/edit', 'WordsController@edit')->middleware('auth');
+// Route::put('/words/{word}', 'WordsController@update')->middleware('auth');
+// Route::delete('/words/{id}', 'WordsController@destroy')->middleware('auth');
 
-Route::get('/words/index', 'WordsController@index')->middleware('auth');
-Route::post('/words/{id}/like', 'WordsController@like')->middleware('auth');
+// Route::get('/words/test', 'WordsController@test')->middleware('auth');
+
+// Route::get('/words/index', 'WordsController@index')->middleware('auth');
+// Route::post('/words/{id}/like', 'WordsController@like')->middleware('auth');
