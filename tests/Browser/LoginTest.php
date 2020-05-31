2e0164095877aa33bbcd9a0e5dbbc8486e156048
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2020-05-31 15:15:47 +0900

    dusk test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -5,9 +5,11 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
 
 class LoginTest extends DuskTestCase
 {
+    use DatabaseMigrations;
     /**
      * A Dusk test example.
      *
@@ -17,7 +19,22 @@ class LoginTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Laravel');
+                    ->assertSee('English Vocabulary Book');
+        });
+    }
+
+    public function testBasicExample()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'taylor@laravel.com',
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertPathIs('/home');
         });
     }
 }
